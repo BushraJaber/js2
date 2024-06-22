d6ba3117b93ca7f7e4b27dfb5708b967e7efb6a0
Eng Bushra Jaber <jaberbushra828@gmail.com>
2024-06-22 09:37:52 +0300

    update

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var data=" ";
-data += `<li>c++</li>`
-data += `<li>java</li>`
-data += `<li>css</li>`
-console.log(data);
+var lang=['c++' , 'java' , 'css'];
+for (var i=0; i<lang.length; i++){
+    data+= "<li> " + lang[i] + "</li>";
+console.log(lang[i]);
+}
+document.querySelector("h2").textContent=" My Love languages :";
+
 document.querySelector("ul").innerHTML= data;
